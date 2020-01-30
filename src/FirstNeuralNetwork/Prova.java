f9128762379e04cac073720f783aa61432bea2e2
Matteo <matteo@matteopicasso.it>
2020-01-30 23:08:12 +0100

    up

--- src/FirstNeuralNetwork/Prova.java
+++ src/FirstNeuralNetwork/Prova.java
@@ -15,10 +15,10 @@ public class Prova {
 
 	public static void main(String[] args) throws FileNotFoundException {
 
-		int features = 30; //setting per scegliere se usare tutte e 30 le features o solamente le 2 piu importanti
+		int features = 2; //setting per scegliere se usare tutte e 30 le features o solamente le 2 piu importanti
 		double lRate = 0.01;
-		double fattore = 0.01; // usato per trasformare il set di dati
-		
+		double fattore = 0.1; // usato per trasformare il set di dati
+		int iterazioni = 1000000; // numero di iterazioni
 		List<Double> costi = new ArrayList<Double>();
 		List<ArrayList<Double>> myList = new ArrayList<ArrayList<Double>>();
 		File csvFile = new File("train.csv");
@@ -42,13 +42,14 @@ public class Prova {
 			e1.printStackTrace();
 		}
 		
+		//neurone
 		Neurone p = new Neurone(features, lRate);
 
 		System.out.println(p.toString());
 		Random r = new Random();
 		double[] point = new double[30];
 
-		long iterazioni = 10000; // numero di iterazioni
+		
 
 		if (features == 30) {
 			for (int i = 0; i < iterazioni; i++) {
@@ -71,7 +72,7 @@ public class Prova {
 				point[0] = myList.get(rd).get(11) * fattore;
 				point[1] = myList.get(rd).get(28) * fattore;
 
-				if (i % 100 == 0) {
+				if (i % 10000 == 0) {
 					double a = p.train(point, myList.get(rd).get(0));
 					costi.add(a);
 				} else {
@@ -79,6 +80,7 @@ public class Prova {
 				}
 			}
 		}
+		
 		myList.clear();
 		csvFile = new File("test.csv"); //si dia per noto la presenza di 66 tumori maligni nel file.
 		csvReader = new BufferedReader(new FileReader(csvFile));
@@ -99,11 +101,13 @@ public class Prova {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-
+		int positiveCounter=0;
 		int mal = 0, ben = 0;
 		if (features == 30) {
 			for (int i = 0; i < myList.size(); i++) {
-
+				if(myList.get(i).get(0)==1){
+					positiveCounter++;
+				}
 				for (int j = 1; j < myList.get(i).size(); j++) {
 					point[j - 1] = myList.get(i).get(j) * fattore;
 				}
@@ -117,6 +121,9 @@ public class Prova {
 			for (int i = 0; i < myList.size(); i++) {
 				point[0] = myList.get(i).get(11) * fattore;
 				point[1] = myList.get(i).get(28) * fattore;
+				if(myList.get(i).get(0)==1){
+					positiveCounter++;
+				}
 
 				if (p.test(point, myList.get(i).get(0))) {
 					mal++;
@@ -129,10 +136,14 @@ public class Prova {
 		System.out.println("Train: "+iterazioni);
 		System.out.println("Lr: "+lRate);
 		System.out.println("Feature: "+features);
-		System.out.println("Benigni " + ben + " maligni " + mal + "; errati il " + (int)Math.sqrt(Math.pow((((mal - 66) * 100) / 270),2)) + "%"+"->" +(int)Math.sqrt(Math.pow((mal-66),2)));
+		System.out.println("Benigni: " + ben + ", maligni: " + mal + ". Casi analizzati: "+myList.size());
+		System.out.println("Predizioni errate totali "+Math.abs((positiveCounter-mal)-((myList.size()-positiveCounter)-ben))+" su "+myList.size()+" ~"
+		+Math.abs((((positiveCounter-mal)-((myList.size()-positiveCounter)-ben))*100)/myList.size()) +"%" );
+		System.out.println( "Maligni errati:" +Math.abs(positiveCounter-mal)+"/" + positiveCounter + " ovvero ~" +
+		Math.abs((((mal - positiveCounter) * 100) /positiveCounter)) +"%");
 		GraphPanel a = new GraphPanel(costi);
 		a.setPreferredSize(new Dimension(1200, 600));
-		JFrame frame = new JFrame("Accuratezza (costo)");
+		JFrame frame = new JFrame("Errore quadratico");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(a);
 		frame.pack();
@@ -140,3 +151,4 @@ public class Prova {
 		frame.setVisible(true);
 	}
 }
+
